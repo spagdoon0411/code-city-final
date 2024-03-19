a4b4d4e0fbbbd1ac9afab04282fc68a2f88c6572
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-03-19 07:17:18 -0700

    define viewport interface using abstract class

--- src/main/java/org/example/TestViewportAndJson.java
+++ src/main/java/org/example/TestViewportAndJson.java
@@ -9,6 +9,8 @@ public class TestViewportAndJson {
     @Test
     public void SimpleTest1()
     {
+        /* We'd use a factory method to obtain and build a list of buildings somewhere. We
+        * probably wouldn't call this exact method, however. */
         TempBuildingInfoFactory fact = new TempBuildingInfoFactory();
         TempBuildingInfo b1 = fact.getCurrentTestBuilding();
         TempBuildingInfo b2 = fact.getCurrentTestBuilding();
@@ -19,11 +21,14 @@ public class TestViewportAndJson {
         testBuildingInfos.add(b2);
         testBuildingInfos.add(b3);
 
-        /* This defines how to encode buildings into JSON. */
+        /* This defines how to encode buildings into JSON and follows the dependency
+        * injection pattern. */
         TempJSONEncoder encoder = new TempJSONEncoder();
 
-        /* A Viewport takes BuildingInfos and a class that encodes them. */
-        Viewport v = new Viewport(testBuildingInfos, encoder);
+        /* A Viewport takes BuildingInfos and a class that encodes them. The viewport
+        * follows the facade pattern by abstracting away the (quite complex!) details of
+        * the 3D renderer implementation. */
+        UnityViewport v = new UnityViewport(testBuildingInfos, encoder);
 
         v.open();
     }
--- src/main/java/org/example/Viewport.java
+++ src/main/java/org/example/Viewport.java
@@ -1,132 +1,13 @@
 package org.example;
 
-import org.apache.commons.io.FileUtils;
-
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
-public class Viewport {
-
-    private static final Platform platform = Platform.MAC;
-
-    private static int idAssignCounter = 0;
-
-    private File viewportExe;
-
-    private final int id;
-
-    public int getId()
-    {
-        return this.id;
-    }
-
-    private final List<TempBuildingInfo> buildingInfos;
-
-    private final String templateExePath;
-
-    private IEncoder encoder;
+public abstract class Viewport {
 
     public Viewport(List<TempBuildingInfo> buildingInfos, IEncoder encoder)
     {
-        this.id = idAssignCounter++;
-        this.buildingInfos = buildingInfos;
-        this.templateExePath = "src"
-                + File.separator + "main"
-                + File.separator + "java"
-                + File.separator + "org"
-                + File.separator + "example"
-                + File.separator  + getPlatformExeName();
-
-        this.viewportExe = new File(templateExePath);
-        this.encoder = encoder;
-    }
-
-    private String getPlatformExeName()
-    {
-        String name = "";
-        switch (platform) {
-            case MAC:
-                name = "ViewportMac.app";
-                break;
-            case WINDOWS:
-                name = "ViewportWindows.app";
-                break;
-        }
-        return name;
-    }
-
-    private void clearPriorViewportDirectory(File viewportDirectory)
-    {
-        /* Ensures no viewport directory already exists */
-        if(viewportDirectory.exists())
-        {
-            try {
-                if(viewportDirectory.isDirectory())
-                {
-                    FileUtils.cleanDirectory(viewportDirectory);
-                    FileUtils.deleteDirectory(viewportDirectory);
-                }
-                else if(viewportDirectory.isFile())
-                {
-                    FileUtils.delete(viewportDirectory);
-                }
-            } catch(IOException e)
-            {
-                System.err.println("Could not clear viewport directory.\n" + e.getMessage());
-            }
-        }
-    }
-
-    private void makeTempViewportDirectory(File viewportDirectory)
-    {
-        clearPriorViewportDirectory(viewportDirectory);
-        if(!viewportDirectory.mkdirs())
-        {
-            System.err.println("Temporary directory for viewport could not be created.");
-        }
-    }
-
-    private void launchViewportExeInTempDir(File viewportExe, File viewportDirectory)
-    {
-        /* Launch the viewport instance in the temporary directory. The viewport exe is responsible for
-         * reading the building JSON data in its current working directory, so set the CWD to the
-         * temporary folder, launch the viewport executable, then return to the old working directory. */
-        String previousCWD = System.getProperty("user.dir");
-        System.setProperty("user.dir", viewportDirectory.getAbsolutePath());
-        try {
-            Desktop.getDesktop().open(viewportExe);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.setProperty("user.dir", previousCWD);
 
     }
 
-    private File copyViewportToTempDirectory(File viewportTemplateExe, File viewportDirectory)
-    {
-        File viewportTempExe = new File(viewportDirectory.getPath() + File.separator + "Viewport.app");
-        try {
-            FileUtils.copyDirectory(viewportTemplateExe, viewportTempExe);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return viewportTempExe;
-    }
-
-    public void open() {
-
-        File tempDir = FileUtils.getTempDirectory();
-        File viewportDirectory = new File(tempDir.getPath() + File.separator + "Viewport" + id);
-
-        makeTempViewportDirectory(viewportDirectory);
-
-        File viewportTempExe = copyViewportToTempDirectory(viewportExe, viewportDirectory);
-
-        encoder.encodeBuildingsInDir(buildingInfos, viewportDirectory);
-
-        launchViewportExeInTempDir(viewportTempExe, viewportDirectory);
-    }
+    public abstract void open();
 }
